4c1b321b3a39913fd7f38292e805f3b35117fd6e
Caspar-Brantberger <3tomu24.branca@folkuniversitetet.nu>
2025-03-27 11:16:34 +0100

    added another localhost

--- src/main/java/com/example/demo/MovieController.java
+++ src/main/java/com/example/demo/MovieController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -9,9 +11,11 @@ import java.util.List;
 @RequestMapping("/movies")
 public class MovieController {
 
+    private final WebClient reviewClient;
     MovieService movieService;
 
-    public MovieController(MovieService movieService) {
+    public MovieController(MovieService movieService, WebClient.Builder reviewClient) {
+        this.reviewClient = reviewClient.baseUrl("http://localhost:8082").build();
         this.movieService = movieService;
     }
 
@@ -23,9 +27,8 @@ public class MovieController {
     @GetMapping("/{id}")
     public ResponseEntity<Movie> getAllMovieswithId(@PathVariable Long id){
         Movie movie = movieService.getMovieById(id);
-
-        return ResponseEntity.ok(movie);
-    }
+       return ResponseEntity.ok(movie);
+   }
     @PostMapping
     public ResponseEntity<Movie> addMovie(@RequestBody Movie movie){
         Movie newMovie = movieService.addMovie(movie);
---
+++ src/main/java/com/example/demo/Review.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Review {
+
+    private Long id;
+    private Long movieId;
+    private String comment;
+    private int rating;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
